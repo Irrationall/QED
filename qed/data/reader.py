7a5274836d42d8fc9c30bbda950e3c1577f58851
unknown <juneseo99@naver.com>
2024-01-22 23:10:50 +0900

    from scanpy added

--- qed/data/reader.py
+++ qed/data/reader.py
@@ -1,7 +1,10 @@
 from .structure import geneset
-from typing import List, Optional
+from typing import List, Optional, TYPE_CHECKING
 import pandas as pd
 import time
+
+if TYPE_CHECKING:
+    from anndata import AnnData
 
 
 def calc_time(func):
@@ -70,8 +73,60 @@ def readseurat(file_path: str, sep: str,
 
 
 @calc_time
-def readscanpy() -> List[geneset] :
+def readscanpy(adata: AnnData,
+               adj_pval_cutoff: float = 0.05,
+               lfc_cutoff: float = 0.5,
+               select_top_n: Optional[int] = None,
+               select_order: Optional[str] = 'scores') -> List[geneset] :
+    
+    if 'rank_genes_groups' not in adata.uns:
+        raise ValueError("Run scanpy.tl.rank_genes_groups first.")
     
     setlist = []
+
+    result = adata.uns['rank_genes_groups']
+    groups = result['names'].dtype.names
+
+    new_df = pd.DataFrame()
+
+    for group in groups:
+        
+        temp_df = pd.DataFrame()
+        temp_df['cluster_names'] = pd.Series([group]*len(result['names'][group]))
+        temp_df['names'] = result['names'][group]
+        temp_df['scores'] = result['scores'][group]
+        temp_df['pvals'] = result['pvals'][group]
+        temp_df['pvals_adj'] = result['pvals_adj'][group]
+        temp_df['logfoldchanges'] = result['logfoldchanges'][group]
+        
+        new_df = pd.concat([new_df, temp_df])
+
+    new_df.reset_index(drop=True, inplace=True)
+
+    new_df = new_df[(new_df['logfoldchanges'] > lfc_cutoff) &
+                    (new_df['pvals_adj'] < adj_pval_cutoff)
+                   ]
     
+    if select_top_n is not None :
+        
+        top_df = new_df.groupby('cluster_names').apply(lambda x: x.nlargest(select_top_n, select_order)).reset_index(drop=True)
+        tmp_group = top_df.groupby('cluster_names')['names']
+
+        for name, group in tmp_group:
+            
+            genes = group.tolist()
+            gs = geneset(name=name, genes=genes)
+            setlist.append(gs)
+
+    
+    else :
+        
+        tmp_group = new_df.groupby('cluster_names')['names']
+
+        for name, group in tmp_group:
+            
+            genes = group.tolist()
+            gs = geneset(name=name, genes=genes)
+            setlist.append(gs)
+     
     return setlist
